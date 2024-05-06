c3c41900bdd9b3a7436ef796062a57537b83bfcb
ShardNguyen <21127345@student.hcmus.edu.vn>
2024-05-06 19:41:52 +0700

    init

---
+++ assets/Scripts/Square.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Square')
+export class Square extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
